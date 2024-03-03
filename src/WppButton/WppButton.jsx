dee005d3258cb8a5a417e1382e67f249f2f6d5de
ronpanic <martinchoyx@gmail.com>
2024-03-03 18:28:05 -0300

    wpp button changed, img imported

--- src/WppButton/WppButton.jsx
+++ src/WppButton/WppButton.jsx
@@ -5,7 +5,7 @@ const WppButton = () => {
   return (
     <div>
         <div className='wp-container'>
-                <a href="https://wa.me/541131295068?text=Hola,%20quiero%20hablar%20contigo" target="_blank" rel="noopener noreferrer">
+                <a href="https://wa.me/541131295068?text=Hola,%20vengo%20de%20ver%20tu%20portfolio" target="_blank" rel="noopener noreferrer">
                     <button>Whatsapp Contact</button>
                 </a>
             </div>
